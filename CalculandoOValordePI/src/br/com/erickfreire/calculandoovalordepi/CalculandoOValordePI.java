9d986d2397ac63e4c14c06a2427f0376b3e52bca
Erick Freire <erilefr@gmail.com>
2021-04-21 16:48:49 -0300

    CalculandoOValordePI
    CalculandoOValordePI

---
+++ CalculandoOValordePI/src/br/com/erickfreire/calculandoovalordepi/CalculandoOValordePI.java
@@ -0,0 +1,40 @@
+package br.com.erickfreire.calculandoovalordepi;
+
+/**
+ * Programa em Java que calcula o valor de PI
+ * @author Erick Freire
+ * @version 1 - Criado em 21-04-2021 as 15:52
+ */
+
+public class CalculandoOValordePI {
+
+	public static void main(String[] args) {
+		
+		double pi = 4.0;
+		double n1 = 3.0;
+		
+		for(int i = 1; i <= 200000; i++) {			
+			
+			if(i == 1) {
+				pi = pi - (4.0 / n1);
+			} else {
+				if(i % 2 == 0) {
+					pi = pi + (4.0 / n1);
+				} 
+				
+				if(i % 2 == 1) {
+					
+					pi =pi - (float)(4.0 / n1) ;
+				}
+					    
+			}
+			
+			n1 += 2;
+			
+			System.out.printf("%.2f %n", pi);
+			
+		}
+		
+	}
+
+}
